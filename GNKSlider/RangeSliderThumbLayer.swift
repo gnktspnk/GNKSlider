61d3be29fdfe4e5074dd7baedf04df72358f2ded
Gennadii <gennadii8@gmail.com>
2017-10-31 23:38:27 +0200

    Initial Commit

---
+++ GNKSlider/RangeSliderThumbLayer.swift
@@ -0,0 +1,15 @@
+//
+//  RangeSliderThumbLayer.swift
+//  GNKSlider
+//
+//  Created by Gennadii Tsypenko on 31/10/2017.
+//  Copyright © 2017 Gennadii Tsypenko. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+
+class RangeSliderThumbLayer: CALayer {
+     var highlighted = false
+     weak var rangeSlider: RangeSlider?
+}
---
+++ GNKSlider/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  GNKSlider
+//
+//  Created by Gennadii Tsypenko on 31/10/2017.
+//  Copyright © 2017 Gennadii Tsypenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let rangeSlider = RangeSlider(frame: .zero)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        rangeSlider.backgroundColor = .red
+        view.addSubview(rangeSlider)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        let margin: CGFloat = 54.0
+        let width: CGFloat = 24
+        let height: CGFloat = 300
+        rangeSlider.frame = CGRect(x: margin, y: margin + topLayoutGuide.length,
+                                   width: width, height: height)
+    }
+
+
+}
+
